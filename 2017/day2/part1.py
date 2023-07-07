1adae99100662c82c5a2379d40277c41439757a3
trimotoj <petkotimotej@gmail.com>
2023-07-07 10:07:16 +0200

    part1solution

--- 2017/day2/part1.py
+++ 2017/day2/part1.py
@@ -3,4 +3,4 @@ with open('2017/day2/input') as file:
     for line in file:
         line=list(map(int,line.strip().split()))
         solution+=max(line) - min(line)
-print(solution)
+print(solution) 
